d9fa230ad5f39a1a5a0398d325aa8eae59bbc787
Mikakokeshi <mikakokeshi00124@gmail.com>
2024-06-13 11:49:06 +0900

    fixed Navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,8 +8,8 @@ const Navbar = () => {
         <div className="wrapper">
             {/* <motion.span initial={{opacity:0 , scale:0.5}} animate={{opacity:1, scale:1}} transition={{duration:0.5}}> Mikakokeshi</motion.span> */}
             <div className="social">
-                <a href=""><img src="/linkedin.png" alt="linkedin" /></a>
-                <a href=""><img src="/github.png" alt="github" /></a>
+                <a href="https://github.com/Mikakokeshi" target="blank"><img src="/github.png" alt="github" /></a>
+                <a href="https://www.linkedin.com/in/mikako-iijima/" target="blank"><img src="/linkedin.png" alt="linkedin" /></a>
             </div>
         </div>
     </div>
